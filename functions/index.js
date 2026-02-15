f79aa3b68e7fa7d11e9b9c520149c0c42ddcbcc1
Ahmed Helmy <ahmedmohammed18112003@gmail.com>
2026-02-15 16:44:38 +0200

    fix bug in functions.

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,7 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest, onCall} = require("firebase-functions/v2/https");
-const functions = require("firebase-functions");
+const {onCall} = require("firebase-functions/v2/https");
 const admin = require("firebase-admin")
 const { FieldValue } = require("firebase-admin/firestore");
 const logger = require("firebase-functions/logger");
